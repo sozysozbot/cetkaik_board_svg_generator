30c3d69b6767293fdd2cf14e1a6bfdd72a04f1b0
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-05-16 08:05:18 +0900

    Fixes #3 in an ugly workaround

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,19 @@ const main = (() => {
             tamzo_color: "orange" | "white" | "green" | "blue" | null
         }) {
         const g = document.createElementNS('http://www.w3.org/2000/svg', "g");
+
+        /*
+         * FIXME: 
+         * THIS SHOULD BE `setAttributeNS`, BUT FOR SOME REASON,
+         * CHANGING THIS TO `setAttributeNS` RESULTS IN THE RESULTING BOARD
+         * TO BE PLACED IN A WRONG POSITION.
+         * THE WORKAROUND IS TO USE `setAttribute` INSTEAD OF `setAttributeNS`,
+         * AND TO ALSO USE `g.innerHTML +=` AT LEAST ONCE AFTER APPENDING THE CHILD ELEMENTS.
+         * PRESUMABLY, THIS BRUTAL WAY OF EDITING THE DOM AMENDS THE PROBLEM
+         * CAUSED BY USING `setAttribute` INSTEAD OF `setAttributeNS`.
+         */
         g.setAttribute("transform", "translate(-41.069 -78.153)");
+
         const tamnua_ = o.tamnua_background ? '#87aade' : 'none';
         const tamhue_ = o.tamhue_background ? '#ffb380' : 'none';
         function square_inv(column: Column, row: Row, fill: string) {
@@ -86,6 +98,13 @@ const main = (() => {
         for (let i = 0; i < input.length; i++) {
             g.appendChild(piece(...(input[i])));
         }
+
+
+        /*
+         * FIXME: THIS ONE IS REQUIRED BY THE WORKAROUND MENTIONED ABOVE
+         */
+        g.innerHTML += "";
+
         if (o.water_edge) {
             // ZAI の水端マーク1
             g.innerHTML += `<path d="m128.05 206.49v3.449h-3.449" fill="none" stroke="#000" stroke-width=".5" />`;
